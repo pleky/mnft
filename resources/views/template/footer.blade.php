12ebf0c09dfcbbb770e382812b8a69df72b3fa3d
Wreda Prasetiyo W <wredaaa@gmail.com>
2021-05-11 12:57:09 +0700

    fix code#3

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -85,7 +85,7 @@
       </footer>
       <!-- end footer -->
 
-    <script src="./index.js"></script>
+    <script src="{{ mix('assets/js/index.js') }}"></script>
     <script>
       feather.replace()
     </script>
