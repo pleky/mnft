f850a46d91f63ee4df39c4f3920f13c7da7881df
ony arianto <pleky@onys-MacBook-Pro.local>
2021-05-19 10:09:28 +0700

    add meta tag in admin page

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -3,6 +3,16 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta property="og:locale" content="en_US">
+  <meta property="og:title" content="About Us - PT Raja Teknik Aditama (RTA)">
+  <meta property="og:type" content="website">
+  <meta property="og:description" content="PT Raja Teknik Aditama specializes in custom-made parts, engineering products and spare&nbsp; parts. We offer high-tech precision machinery parts for specific application toimprove parts lifetime, machine efficiency and productivity. Our flexibilty allow us to provide the best services to our customer for new build or repair parts programme to set the most economical solutions. &nbsp;">
+  <meta property="og:site_name" content="PT Raja Teknik Aditama (RTA)">
+  <meta property="og:url" content="https://rtateknik.com/">
+  <link rel="canonical" href="https://rtateknik.com/">
   <title>RTA | Log in</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
