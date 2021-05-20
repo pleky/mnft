59688c00743013752c0a8a3d1bf5baed3b989e63
ony arianto <pleky@onys-MacBook-Pro.local>
2021-05-20 15:27:45 +0700

    add function remove fieldset

--- resources/views/admin/forms/contentsForm.blade.php
+++ resources/views/admin/forms/contentsForm.blade.php
@@ -36,9 +36,23 @@
                                     {!! Form::myTextarea('description', null, ['class' => 'form-control', 'id' => 'description']) !!}
                                 </div>
                                 <div id='galleryContainer'>
-                                </div>
-                                <div class="form-group">
-                                    <button class='btn btn-info' type="button" id="btnAddGallery">Add Gallery</button>
+                                    <div class="form-group">
+                                        <button class='btn btn-info' type="button" id="btnAddGallery">Add Gallery</button>
+                                    </div>
+                                    <!-- <fieldset class='form-group border p-3'>
+                                        <legend class='w-auto px-2'>
+                                            Gallery
+                                        </legend>
+                                            <div class='form-group'>
+                                                <label for='galleryTitle'>Title</label>
+                                                <input class='form-control' id='galleryTitle' type='text'>
+                                            </div>
+                                            <div class='form-group'>
+                                                <label for='galleryImage'>Image</label>
+                                                <input class='form-control' id='galleryImage' type='file'>
+                                            </div>
+                                        <a class='btn btn-danger text-white' id='btn-remove'>Remove</a>
+                                    </fieldset> -->
                                 </div>
                                 {!! Form::close() !!}
                         </div>
@@ -53,28 +67,74 @@
 @endsection
 
 <script>
-    document.addEventListener("DOMContentLoaded", () => {
-        const templateToAdd = "<fieldset class='form-group border p-3'>\
-                                    <legend class='w-auto px-2'>\
-                                        Gallery\
-                                    </legend>\
-                                    <div>\
-                                        <div class='form-group'>\
-                                            <label for='galleryTitle'>Title</label>\
-                                            <input class='form-control' id='galleryTitle' type='text'>\
-                                        </div>\
-                                        <div class='form-group'>\
-                                            <label for='galleryImage'>Image</label>\
-                                            <input class='form-control' id='galleryImage' type='file'>\
-                                        </div>\
-                                    </div>\
-                                </fieldset>";
-        const container = document.getElementById("form-container");
+    let id = 0;
+
+    const removeElement = (e) => {
         const galleryContainer = document.getElementById("galleryContainer");
+        galleryContainer.removeChild(e);
+    }
+    const addGallery = () => {
+        id++;
+        const galleryContainer = document.getElementById("galleryContainer");
+
+        let fieldset = document.createElement('fieldset');
+        let inputText = document.createElement('input');
+        let inputFile = document.createElement('input');
+        let legend = document.createElement('legend');
+        let textNode = document.createTextNode('Gallery');
+        let formGroupText = document.createElement('div');
+        let formGroupImage = document.createElement('div');
+        let labelText = document.createElement('label');
+        let labelTextNode = document.createTextNode('Title');
+        let labelImage = document.createElement('label');
+        let labelImageNode = document.createTextNode('Image');
+        let buttonRemove = document.createElement('button');
+
+        fieldset.setAttribute('class', 'form-group border p-3');
+
+        legend.setAttribute('class', 'w-auto px-2');
+        legend.innerHTML = 'Gallery';
+
+
+        buttonRemove.setAttribute('class', 'btn btn-danger');
+        buttonRemove.setAttribute('type', 'button');
+        buttonRemove.innerHTML = 'Remove';
+        buttonRemove.setAttribute('id', 'reqsr' + id);
+        buttonRemove.onclick = function() {
+            removeElement(fieldset);
+        };
+
+        inputText.setAttribute('class', 'form-control');
+        inputText.setAttribute('id', 'galleryTitle');
+        labelText.setAttribute('for', 'galleryTitle');
+        formGroupText.setAttribute('class', 'form-group');
+        labelText.appendChild(labelTextNode);
+        formGroupText.appendChild(labelText);
+        formGroupText.appendChild(inputText);
+
+        inputFile.type = 'file';
+        inputFile.setAttribute('class', 'form-control');
+        inputFile.setAttribute('id', 'galleryImage');
+        labelImage.setAttribute('for', 'galleryImage');
+        formGroupImage.setAttribute('class', 'form-group');
+        labelImage.appendChild(labelImageNode);
+        formGroupImage.appendChild(labelImage);
+        formGroupImage.appendChild(inputFile);
+
+        fieldset.appendChild(legend);
+        fieldset.appendChild(formGroupText);
+        fieldset.appendChild(formGroupImage);
+        fieldset.appendChild(buttonRemove);
+
+
+        galleryContainer.appendChild(fieldset);
+
+    }
+
+    document.addEventListener("DOMContentLoaded", () => {
         const btnAddGallery = document.getElementById('btnAddGallery');
         btnAddGallery.addEventListener('click', function() {
-            console.log('jancok')
-            galleryContainer.insertAdjacentHTML('beforeend', templateToAdd);
+            addGallery()
         })
     });
 </script>
