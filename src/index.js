467f844f8a2ce74810faf25128c0618cf20204de
pleky2 <onyarianto03@gmail.com>
2023-08-18 21:08:46 +0700

    show hide

--- src/index.js
+++ src/index.js
@@ -36,11 +36,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const secondNavPanel = document.getElementById("second-nav-panel");
   const secondNavItems = document.querySelectorAll(".second-nav-item");
+  const menu = document.querySelectorAll(".menu");
   const body = document.querySelector("body");
   secondNavItems.forEach((item) => {
     item.addEventListener("mouseenter", function () {
       secondNavPanel.style.visibility = "visible";
     });
+  });
+
+  // hide the second nav panel when the user not hover the second nav panel or the second nav item
+  secondNavPanel.addEventListener("mouseleave", function () {
+    secondNavPanel.style.visibility = "hidden";
   });
 
   body.addEventListener("click", function (e) {
@@ -52,4 +58,32 @@ document.addEventListener("DOMContentLoaded", function () {
       secondNavPanel.style.visibility = "hidden";
     }
   });
+
+  const list = document.getElementById("list");
+  const listChildren = list.children;
+
+  console.log(listChildren);
+
+  for (let i = 0; i < listChildren.length; i++) {
+    // save the id to the localstorage
+    listChildren[i].addEventListener("click", function () {
+      // get the data target of element
+      const getValue = this.getAttribute("data-target");
+      
+      // show the element that has the same id with the data target
+      document.getElementById(getValue).classList.remove("d-none");
+
+      // hide the other element
+      for (let j = 0; j < listChildren.length; j++) {
+        if (listChildren[j].getAttribute("data-target") !== getValue) {
+          document
+            .getElementById(listChildren[j].getAttribute("data-target"))
+            .classList.add("d-none");
+        }
+      }
+      
+    });
+  }
+
+
 });
